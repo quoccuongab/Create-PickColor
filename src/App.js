1d62cf312136094d9b2be944f2d6b77d9c6493a7
unknown <quoccuong.ab62@gmail.com>
2019-05-14 16:24:53 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ColorPicker from './components/ColorPicker.js'
+import Reset from './components/Reset.js'
+import Result from './components/Result.js'
+import SizeSetting from './components/SizeSetting.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      color : 'red',
+      fontSize :12
+    }
+    this.onSetColor=this.onSetColor.bind(this);
+    this.onChangeSize = this.onChangeSize.bind(this);
+  }
+
+  onSetColor(params){
+    this.setState({
+      color :params
+    })
+  }
+  onChangeSize(value){
+    if(this.state.fontSize +value >= 8 && this.state.fontSize +value <=36){
+      this.setState({
+        fontSize : this.state.fontSize +value
+
+      });
+    
+  } 
+ 
+  }
+ render(){
+        return (
+      <div className='container mt-50'>
+        <div className='row'>
+              <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor}/>
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+           <SizeSetting fontSize  ={this.state.fontSize}  
+            onChangeSize={this.onChangeSize}/>
+       
+           <Reset/>
+          </div>
+         
+           <Result color={this.state.color} fontSize={this.state.fontSize}/>
+          </div>
+        </div>
+      
+    )﻿
+      }
+
+ }
 export default App;
